b6401e1284237747c253d00bc8d99cb7e1523f4e
dongyeuk <yupab3@naver.com>
2025-07-08 14:35:21 +0900

    upload

---
+++ backend/src/test/java/com/dongyeuk/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyeuk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
